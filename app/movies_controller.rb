fec82d7042710742a1966ef060b26af86859926c
Alex Merecka <merecka@gmail.com>
2019-05-31 23:37:21 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -38,19 +38,19 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.first
 end
 
 def can_find_by_multiple_attributes
@@ -58,28 +58,31 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by title: 'Title'
+  Movie.find_by release_date: 2000
+  Movie.find_by director: 'Me'
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  __
+  Movie.where("release_date > '2002'").order("release_date DESC")
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+  movie = Movie.find_by title: 'Awesome Flick'
+  movie.title = "Even Awesomer Flick"
+  movie.save
+  movie
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  movie = Movie.find_by(title: 'Wat')
+  movie.update(title: 'Wat, huh?')
 end
 
 def can_update_multiple_items_at_once
