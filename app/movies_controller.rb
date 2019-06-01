84a187bca5e1e4ef6182dab2a9cad9f8bfcf7845
Alex Merecka <merecka@gmail.com>
2019-06-01 00:01:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -97,7 +97,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  move = Movie.find_by(title: 'That One Where the Guy Kicks Another Guy Once')
   __
 end
 
